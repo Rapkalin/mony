8e355e48e589bc0f17193371d3a9fe0ebbd0da33
Rapkalin <raphael@myminicom.com>
2025-01-08 18:42:36 +0100

    Init branch with controller and template

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $result = $this->calcul(2);
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'sum' => $result
+        ]);
+    }
+
+    private function calcul(int $int): int
+    {
+        return $int*2;
+    }
+}
