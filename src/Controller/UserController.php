72028e556dd9a916a1ed5a968662d22d13ac6192
Rapkalin <raphael@myminicom.com>
2025-04-27 19:42:01 +0200

    feat(MNY-2): add user login and dahsbord template

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class UserController extends BaseController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(#[CurrentUser] User $user): Response
+    {
+        return $this->render('user/view.html.twig', [
+            'user' => [
+                'id' => $user->getId(),
+                'name' => $user->getUsername(),
+            ],
+        ]);
+    }
+}
